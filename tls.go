97d0c0ed1568ece9071cd83d0f441d83d365b251
SinTan1071 <297085213@qq.com>
2018-03-23 16:17:59 +0800

    debug

--- tls.go
+++ tls.go
@@ -1,100 +1,100 @@
-package main
-
-import (
-	"io"
-	"net/http"
-	"log"
-)
-func HelloServer(w http.ResponseWriter, req *http.Request) {
-	io.WriteString(w, "hello, world!\n")
-}
-func main() {
-	http.HandleFunc("/hello", HelloServer)
-	err := http.ListenAndServeTLS(":8080", "cert.pem", "key.pem", nil)
-	if err != nil {
-		log.Fatal("ListenAndServe: ", err)
-	}
-}
-
-
-// tls server
-
-package main
-import (
-"log"
-"crypto/tls"
-"net"
-"bufio"
-)
-func main() {
-	log.SetFlags(log.Lshortfile)
-	cer, err := tls.LoadX509KeyPair("cert.pem", "key.pem")
-	if err != nil {
-		log.Println(err)
-		return
-	}
-	config := &tls.Config{Certificates: []tls.Certificate{cer}}
-	ln, err := tls.Listen("tcp", ":8000", config)
-	if err != nil {
-		log.Println(err)
-		return
-	}
-	defer ln.Close()
-	for {
-		conn, err := ln.Accept()
-		if err != nil {
-			log.Println(err)
-			continue
-		}
-		go handleConnection(conn)
-	}
-}
-func handleConnection(conn net.Conn) {
-	defer conn.Close()
-	r := bufio.NewReader(conn)
-	for {
-		msg, err := r.ReadString('\n')
-		if err != nil {
-			log.Println(err)
-			return
-		}
-		println(msg)
-		n, err := conn.Write([]byte("world\n"))
-		if err != nil {
-			log.Println(n, err)
-			return
-		}
-	}
-}
-
-//tls client
-
-package main
-import (
-"log"
-"crypto/tls"
-)
-func main() {
-	log.SetFlags(log.Lshortfile)
-	conf := &tls.Config{
-		InsecureSkipVerify: true,
-	}
-	conn, err := tls.Dial("tcp", "127.0.0.1:8000", conf)
-	if err != nil {
-		log.Println(err)
-		return
-	}
-	defer conn.Close()
-	n, err := conn.Write([]byte("hello\n"))
-	if err != nil {
-		log.Println(n, err)
-		return
-	}
-	buf := make([]byte, 100)
-	n, err = conn.Read(buf)
-	if err != nil {
-		log.Println(n, err)
-		return
-	}
-	println(string(buf[:n]))
-}
+//package main
+//
+//import (
+//	"io"
+//	"net/http"
+//	"log"
+//)
+//func HelloServer(w http.ResponseWriter, req *http.Request) {
+//	io.WriteString(w, "hello, world!\n")
+//}
+//func main() {
+//	http.HandleFunc("/hello", HelloServer)
+//	err := http.ListenAndServeTLS(":8080", "cert.pem", "key.pem", nil)
+//	if err != nil {
+//		log.Fatal("ListenAndServe: ", err)
+//	}
+//}
+//
+//
+//// tls server
+//
+//package main
+//import (
+//"log"
+//"crypto/tls"
+//"net"
+//"bufio"
+//)
+//func main() {
+//	log.SetFlags(log.Lshortfile)
+//	cer, err := tls.LoadX509KeyPair("cert.pem", "key.pem")
+//	if err != nil {
+//		log.Println(err)
+//		return
+//	}
+//	config := &tls.Config{Certificates: []tls.Certificate{cer}}
+//	ln, err := tls.Listen("tcp", ":8000", config)
+//	if err != nil {
+//		log.Println(err)
+//		return
+//	}
+//	defer ln.Close()
+//	for {
+//		conn, err := ln.Accept()
+//		if err != nil {
+//			log.Println(err)
+//			continue
+//		}
+//		go handleConnection(conn)
+//	}
+//}
+//func handleConnection(conn net.Conn) {
+//	defer conn.Close()
+//	r := bufio.NewReader(conn)
+//	for {
+//		msg, err := r.ReadString('\n')
+//		if err != nil {
+//			log.Println(err)
+//			return
+//		}
+//		println(msg)
+//		n, err := conn.Write([]byte("world\n"))
+//		if err != nil {
+//			log.Println(n, err)
+//			return
+//		}
+//	}
+//}
+//
+////tls client
+//
+//package main
+//import (
+//"log"
+//"crypto/tls"
+//)
+//func main() {
+//	log.SetFlags(log.Lshortfile)
+//	conf := &tls.Config{
+//		InsecureSkipVerify: true,
+//	}
+//	conn, err := tls.Dial("tcp", "127.0.0.1:8000", conf)
+//	if err != nil {
+//		log.Println(err)
+//		return
+//	}
+//	defer conn.Close()
+//	n, err := conn.Write([]byte("hello\n"))
+//	if err != nil {
+//		log.Println(n, err)
+//		return
+//	}
+//	buf := make([]byte, 100)
+//	n, err = conn.Read(buf)
+//	if err != nil {
+//		log.Println(n, err)
+//		return
+//	}
+//	println(string(buf[:n]))
+//}
